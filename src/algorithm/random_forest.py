f35cada044be55063f4e7697e3fb035477aaa466
popoulpe <54855619+popoulpe@users.noreply.github.com>
2025-05-03 16:23:15 +0200

    rf -> proba de chaque nombre + stockage entrainement

--- src/algorithm/random_forest.py
+++ src/algorithm/random_forest.py
@@ -1,18 +1,32 @@
 import pandas as pd
+import numpy as np
+import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
-#TODO: stocker la version entrainée pour ne pas réentrainer a chaque fois et accélérer le temps d'exécution
+#TODO: mesurer le temps d'execution 
 
-def train_and_predict(x_train, y_train, x_test):
+MODEL_PATH = "resources/models/rf_model.pkl"
 
+def train(x_train, y_train):
     clf = RandomForestClassifier()
     clf.fit(x_train, y_train)
+    joblib.dump(clf, MODEL_PATH)
+    print("Model trained and saved.")
 
-    #predict value of label using classifier
-    prediction_test = clf.predict(x_test)
-    return prediction_test
+def predict(x_test):
+    
+    clf = joblib.load(MODEL_PATH)
+    prediction_test = clf.predict_proba(x_test)
+    
+    valeurProbable = []
+    
+    for i in range(len(prediction_test)):
+        predicted_label = prediction_test[i].argmax()
+        valeurProbable.append(predicted_label)
+
+    return prediction_test, valeurProbable
 
 if __name__ == "__main__":
 
@@ -22,8 +36,11 @@ if __name__ == "__main__":
     x_train,y_train=train_file.iloc[:,1:].values,train_file.iloc[:,0].values
     x_test,y_test=test_file.iloc[:,1:].values,test_file.iloc[:,0].values
 
-    prediction_test = train_and_predict(x_train, y_train, x_test)
+    #enlever le # pour réentrainer le programme 
+    #train(x_train, y_train)
 
-    
-    print("\ntest precision: ", accuracy_score(y_test, prediction_test))
-    print("test matrice de confusion: \n", confusion_matrix(y_test, prediction_test))
+    rslt_rf = predict(x_test)
+
+    print("", rslt_rf[0][1])
+    print("\ntest precision: ", accuracy_score(y_test, rslt_rf[1]))
+    print("test matrice de confusion: \n", confusion_matrix(y_test, rslt_rf[1]))
