503001336b3bd4e982abe05b80366d5928a083df
omar karam <oa12142001@gmai.comgit config --global user.email oa12142001@gmai.comgit config --global user.email oa12142001@gmai.com>
2025-05-08 15:56:49 +0200

    LR

--- src/algorithm/logistic_regression.py
+++ src/algorithm/logistic_regression.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import joblib
+import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-
-#besoin de normaliser les données 
+#besoin de normaliser les données
 from sklearn.preprocessing import StandardScaler
-
-#TODO: stocker la version entrainée pour ne pas réentrainer a chaque fois et accélérer le temps d'exécution
 
 MODEL_PATH = "resources/models/lr_model.pkl"
 
 def train(x_train, y_train):
-    #max_iter créé une erreur comme étant trop bas, mettre en param "max_iter=10000" 
+    #max_iter créé une erreur comme étant trop bas, mettre en param "max_iter=10000"
     #pour ne plus avoir l'erreur, semble avoir un impact negatif sur le code
     clf = LogisticRegression()
     clf.fit(x_train, y_train)
@@ -20,12 +18,12 @@ def train(x_train, y_train):
     print("Model trained and saved.")
 
 def predict(x_test):
-    
+
     clf = joblib.load(MODEL_PATH)
     prediction_test = clf.predict_proba(x_test)
-    
+
     valeurProbable = []
-    
+
     for i in range(len(prediction_test)):
         predicted_label = prediction_test[i].argmax()
         valeurProbable.append(predicted_label)
@@ -33,10 +31,15 @@ def predict(x_test):
     return prediction_test, valeurProbable
 
 
-if __name__ == "__main__":
+if name == "main":
 
-    test_file = pd.read_csv("resources/test_data.csv")
-    train_file = pd.read_csv("resources/train_data.csv")
+    test_file = "resources/data_utilisees/test_data.pkl"
+    train_file = "resources/data_utilisees/train_data.pkl"
+
+    with open(test_file, "rb") as f:
+        df_test = pickle.load(f)
+    with open(train_file, "rb") as f:
+        df_train = pickle.load(f)
 
     x_train,y_train=train_file.iloc[:,1:].values,train_file.iloc[:,0].values
     x_test,y_test=test_file.iloc[:,1:].values,test_file.iloc[:,0].values
@@ -45,7 +48,7 @@ if __name__ == "__main__":
     x_train = scaler.fit_transform(x_train)
     x_test = scaler.transform(x_test)
 
-    #enlever le # pour réentrainer le programme 
+    #enlever le # pour réentrainer le programme
     train(x_train, y_train)
 
     rslt_rf = predict(x_test)
